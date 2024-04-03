71cd8c98c83207b3cfe527dcd56d5d5cf8b87f9f
Rahul Patwal <patwalrahul3@gmail.com>
2024-04-03 23:30:23 +0530

    navigate programatically

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+
+  constructor(private router: Router){
+
+  }
+
+
+
+
+  navigate(){
+
+    // this.router.navigateByUrl('/categories')
+    this.router.navigate(['/categories'])
+
+  }
 
 }
