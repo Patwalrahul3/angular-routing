9f1baf99051da4c904a2684b2497234af4f8685b
Rahul Patwal <patwalrahul3@gmail.com>
2024-04-03 23:07:46 +0530

    boiler code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { UserComponent } from './user/user.component';
+import {  RouterModule, Routes } from '@angular/router';
+
+const appRoutes : Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'user' , component: UserComponent},
+  {path: 'categories' , component: CategoriesComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoriesComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
