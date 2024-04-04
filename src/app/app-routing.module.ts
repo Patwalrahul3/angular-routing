507422d6a4d2c854e2b58070ce9ad1eb4e8c05a2
Rahul Patwal <patwalrahul3@gmail.com>
2024-04-04 22:41:54 +0530

    separate file for approuting module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes : Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'users' , component: UsersComponent ,
+   children:[{path: ':id/:name' , component: UserComponent}],
+  },
+ 
+  {path: 'categories' , component: CategoriesComponent},
+  {path: 'page-not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: 'page-not-found'}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { UserComponent } from './user/user.component';
-import {  RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes : Routes = [
-  {path: '' , component: HomeComponent},
-  {path: 'users' , component: UsersComponent ,
-   children:[{path: ':id/:name' , component: UserComponent}],
-  },
- 
-  {path: 'categories' , component: CategoriesComponent},
-  {path: 'page-not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: 'page-not-found'}
-]
 
 @NgModule({
   declarations: [
@@ -33,7 +22,6 @@ const appRoutes : Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
