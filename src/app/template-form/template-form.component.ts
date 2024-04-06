3691c8a7424305140b5594401305a5881cad9aeb
Rahul Patwal <patwalrahul3@gmail.com>
2024-04-06 23:16:03 +0530

    formsubmit and reset in tdf

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -4,42 +4,58 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.css']
+  styleUrls: ['./template-form.component.css'],
 })
 export class TemplateFormComponent {
-
-  gender =  "male"
+  @ViewChild('f') signupForm!: NgForm;
+  gender = 'male';
   about!: string;
+  submitted = false;
 
-@ViewChild('f') signupForm!: NgForm;
+
+
+  user = {
+    username: '',
+    email: '',
+    gender: '',
+    about: ''
+  };
+
+
+  onFormSubmit() {
   
-onFormSubmit(){
-    console.log(this.signupForm);
-    
+    // console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.gender = this.signupForm.value.gender;
+    this.user.about = this.signupForm.value.about;
 
+    // reset
+    this.signupForm.reset();
+    
   }
 
   // setValue
-  // fillValues(){
-  //   this.signupForm.form.setValue({
-  //     userData : {
-  //       username: "Rahul Patwal",
-  //       email: "patwalrahul3@gmail.com"
-  //     },
-  //     gender: 'male',
-  //     about: 'ALL Rounder'
-  //   })
-  // }
-  // patchValue
   fillValues(){
-    this.signupForm.form.patchValue({
+    this.signupForm.form.setValue({
       userData : {
         username: "Rahul Patwal",
         email: "patwalrahul3@gmail.com"
       },
-      // gender: 'male',
-      // about: 'ALL Rounder'
+      gender: 'male',
+      about: 'ALL Rounder'
     })
   }
-
+  // patchValue
+  // fillValues() {
+  //   this.signupForm.form.patchValue({
+  //     userData: {
+  //       username: 'Rahul Patwal',
+  //       email: 'patwalrahul3@gmail.com',
+  //     },
+  //     // gender: 'male',
+  //     // about: 'ALL Rounder'
+  //   });
+  // }
 }
