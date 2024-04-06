fe02d70dc39e25e2d36d40d491d4461fe96d86cc
Rahul Patwal <patwalrahul3@gmail.com>
2024-04-06 23:50:40 +0530

    validator and error message in reactive form

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.css']
 })
-export class ReactiveComponent {
+export class ReactiveComponent { 
 
   signupForm!: FormGroup;
 
@@ -18,10 +18,15 @@ export class ReactiveComponent {
 
   ngOnInit(){
     this.signupForm = new FormGroup({
-      'username': new FormControl('null'),
-      'email': new FormControl('null'),
+      'username': new FormControl('', Validators.required),
+      'email': new FormControl('', [Validators.required, Validators.email]),
       'gender': new FormControl('female'),
     })
+  }
+
+
+  onSubmit(){
+    
   }
 
 }
