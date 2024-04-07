65f76306533551640e7255bb4b7e7b5f7b29517b
Rahul Patwal <patwalrahul3@gmail.com>
2024-04-07 11:01:01 +0530

    custom validation double entry in name

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -9,34 +9,48 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class ReactiveComponent {
   signupForm!: FormGroup;
 
+  DuplicatedName = ['rahul', 'manish'];
+
   genders = ['male', 'female'];
 
   constructor() {}
 
-  get hobbiesControl(){
-     return (<FormArray>this.signupForm.get('hobbies')).controls
+  get hobbiesControl() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl('', Validators.required),
+        username: new FormControl('', [
+          Validators.required,
+          this.IsDuplicatedRestrictedName.bind(this),
+        ]),
         email: new FormControl('', [Validators.required, Validators.email]),
       }),
 
       gender: new FormControl('female'),
-      hobbies: new FormArray([])
+      hobbies: new FormArray([]),
     });
   }
 
   onSubmit() {
-    console.log(this.signupForm.value)
+    console.log(this.signupForm);
+    console.log(this.signupForm.value);
   }
 
-  addHobby(){
-
-    const control = new FormControl('',[Validators.required]);
+  addHobby() {
+    const control = new FormControl('', [Validators.required]);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
-    
+  }
+
+  IsDuplicatedRestrictedName(
+    controls: FormControl
+  ): { [s: string]: boolean } | null {
+    if (this.DuplicatedName.includes(controls.value)) {
+      return { nameIsRestricted: true };
+    }
+
+    return null;
   }
 }
